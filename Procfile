14e815918586fe02958ce746d14e466b629ac381
Rachel Kenny <50603339+rachellkenny@users.noreply.github.com>
2019-12-18 10:18:05 -0500

    env vars changed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node db.js
--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 // opens connection to database
 // connects to db before app runs
 const mongodb = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
 const port = process.env.PORT || 3000;
 
-const connectionString =
-  "mongodb+srv://rkenny:rkenny@webapp-fwa0k.mongodb.net/BookExchange?retryWrites=true&w=majority";
-
 mongodb.connect(
-  connectionString,
+  process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function(err, client) {
     module.exports = client;
     const app = require("./server");
-    app.listen(3000, () => {
+    app.listen(port, () => {
       console.log("Server running on port " + port);
     });
   }
